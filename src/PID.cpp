d523e5311aba44752528113151ef26eb40649c2f
Unknown <elias@zimi.de>
2018-04-24 23:28:17 +0200

    Setup PID class

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
+* the PID class.
 */
 
 PID::PID() {}
@@ -11,11 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  /* Set Errors to zero */
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+  /* Set coefficients to one */
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte; /* Note: update p_error after d_error is updated with previous value */
 }
 
 double PID::TotalError() {
+  /* Sum of all Errors p, i, d */
+  return (d_error + i_error + p_error);
 }
 
